c70237a8a31b507c42c7bc0c4a32d6475019556e
imnodb <imnodb@gmail.com>
2023-09-25 23:08:58 +0800

    allArts

--- src/routers/Starrail.js
+++ src/routers/Starrail.js
@@ -520,6 +520,8 @@ function Starrail() {
 
       <artContext.Provider value={setArt}>
         <ArtifactModal art={art} setArt={setArt}></ArtifactModal>
+        <h1>总数：{allArts.length}</h1>
+        <h2>需要分解数量：{allArts.filter(a => !a.save).length}</h2>
         <Tabs defaultActiveKey="1" size="large" type="card" items={items} />
       </artContext.Provider>
     </div>
