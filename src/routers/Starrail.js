e9c3dff21c538649b9299dbeedcb1ed2d0bf2a47
imnodb <imnodb@gmail.com>
2023-09-25 23:14:06 +0800

    需要分解数量

--- src/routers/Starrail.js
+++ src/routers/Starrail.js
@@ -521,7 +521,7 @@ function Starrail() {
       <artContext.Provider value={setArt}>
         <ArtifactModal art={art} setArt={setArt}></ArtifactModal>
         <h1>总数：{allArts.length}</h1>
-        <h2>需要分解数量：{allArts.filter(a => !a.save).length}</h2>
+        <h2>需要分解数量：{allArts.filter(a => a.level === 0 && !a.save).length}</h2>
         <Tabs defaultActiveKey="1" size="large" type="card" items={items} />
       </artContext.Provider>
     </div>
